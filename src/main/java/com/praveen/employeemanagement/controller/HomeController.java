51a6f85202162bab76a5c25b5fb3e19b72b2bb38
praveensenthil-1123 <praveensenthil1123@gmail.com>
2026-08-05 18:25:42 +0530

    Add HomeController for employee management app

---
+++ src/main/java/com/praveen/employeemanagement/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.praveen.employeemanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Employee Management Application is Running";
+    }
+}
